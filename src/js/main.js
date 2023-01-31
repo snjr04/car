726e459af4c258eaf208b7060bbaaec26200a4d5
snjr04 <superkingbro004@gmail.com>
2023-01-31 09:32:20 +0300

    'new'

--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,19 @@
 
-let callBtn = document.querySelector('.call')
-let overlay = document.querySelector('.overlay')
+let callBtn = document.querySelector('.call');
+let overlay = document.querySelector('.overlay');
 
-let closeBtn = document.querySelector('.overlay__close')
+let closeBtn = document.querySelector('.overlay__close');
 
 callBtn.onclick = function () {
     overlay.style.display = 'flex'
-}
+};
 
 closeBtn.onclick = function () {
     overlay.style.display = 'none'
-}
+};
 
 overlay.onclick = function (event) {
     if (event.target.classList.contains('overlay')) {
         overlay.style.display = 'none'
     }
-}
+};
